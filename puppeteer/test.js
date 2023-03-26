c74967ad3cff50c10054b68a14ef87d32fc4c12f
root <root@fffedaefbcbdfbfabebcfadeaacdbeedfafa-0.fffedaefbcbdfbfabebcfadeaacdbeedfafa.che.svc.cluster.local>
2023-03-26 18:01:24 +0000

    Backup Branch before deleting

---
+++ puppeteer/test.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fffedaefbcbdfbfabebcfadeaacdbeedfafa.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('h1');
+    console.log('TESTCASE:testcase1:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase1:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
